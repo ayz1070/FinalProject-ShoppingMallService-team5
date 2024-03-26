b9445fc6fe88e8007427ffe074a9a6894df73c1f
Jun <ayz1070@gmail.com>
2024-03-26 16:31:54 +0900

    [#6] 게시판에 필요한 리소스 파일 추가 및 설정

--- MungNolZa/app/src/main/java/kr/co/lion/mungnolza/ui/main/MainActivity.kt
+++ MungNolZa/app/src/main/java/kr/co/lion/mungnolza/ui/main/MainActivity.kt
@@ -1,12 +1,31 @@
 package kr.co.lion.mungnolza.ui.main
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.google.android.material.transition.MaterialSharedAxis
 import kr.co.lion.mungnolza.R
+import kr.co.lion.mungnolza.databinding.ActivityMainBinding
+import kr.co.lion.mungnolza.ui.freeboard.BoardActivity
 
 class MainActivity : AppCompatActivity() {
+   lateinit var binding:ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+
+        binding.buttonTest2.setOnClickListener {
+            val intent = Intent(this,BoardActivity::class.java)
+            startActivity(intent)
+        }
+
+        setContentView(binding.root)
+
     }
+
 }
