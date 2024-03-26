1bf818bea5a6e44316b227e259439a04ef19ca05
chanho <uni070@naver.com>
2024-03-26 15:06:19 +0900

    first commit

---
+++ MungNolZa/app/src/main/java/kr/co/lion/mungnolza/ui/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package kr.co.lion.mungnolza.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kr.co.lion.mungnolza.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
